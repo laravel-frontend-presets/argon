0b60f472a71d68588fdedeb91914ab31be869c83
Updivision Team <45566836+teamupdivision@users.noreply.github.com>
2020-04-01 12:59:47 +0300

    Update sidebar.blade.php

--- src/argon-stubs/resources/views/layouts/navbars/sidebar.blade.php
+++ src/argon-stubs/resources/views/layouts/navbars/sidebar.blade.php
@@ -125,7 +125,7 @@
                         <i class="ni ni-circle-08 text-pink"></i> {{ __('Register') }}
                     </a>
                 </li>
-                <li class="nav-item mb-5 bg-danger" style="position: absolute; bottom: 0;">
+                <li class="nav-item mb-5 mr-4 ml-4 pl-1 bg-danger" style="position: absolute; bottom: 0;">
                     <a class="nav-link text-white" href="https://www.creative-tim.com/product/argon-dashboard-pro-laravel" target="_blank">
                         <i class="ni ni-cloud-download-95"></i> Upgrade to PRO
                     </a>
