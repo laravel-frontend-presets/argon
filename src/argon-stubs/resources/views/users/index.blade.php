eb7186b6a79559baac814003cdcfe77840ef8fc4
Updivision Team <45566836+teamupdivision@users.noreply.github.com>
2020-04-08 12:51:15 +0300

    Update index.blade.php

--- src/argon-stubs/resources/views/users/index.blade.php
+++ src/argon-stubs/resources/views/users/index.blade.php
@@ -147,11 +147,11 @@
                     <i class="ni ni-circle-08 text-pink"></i> Register
                 </a>
             </li>
-            <li class="nav-item mb-5" style="position: absolute; bottom: 0;">
-                <a class="nav-link" href="https://www.creative-tim.com/product/argon-dashboard-pro-laravel" target="_blank">
-                    <i class="ni ni-cloud-download-95"></i> Upgrade to PRO
-                </a>
-            </li>
+                           <li class="nav-item mb-5 mr-4 ml-4 pl-1 bg-danger" style="position: absolute; bottom: 0;">
+                    <a class="nav-link text-white" href="https://www.creative-tim.com/product/argon-dashboard-pro-laravel" target="_blank">
+                        <i class="ni ni-cloud-download-95"></i> Upgrade to PRO
+                    </a>
+                </li>
         </ul>
         <!-- Divider -->
         <hr class="my-3">
